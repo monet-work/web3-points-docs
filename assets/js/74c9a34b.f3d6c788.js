f6860ed5bbd12c76ba28c9f7223501273db986b6
anujkmonetwork <126776426+anujkmonetwork@users.noreply.github.com>
2024-06-14 20:02:27 +0530

    NEW ASSETS

---
+++ assets/js/74c9a34b.f3d6c788.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpoints=self.webpackChunkpoints||[]).push([[3260],{5911:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/web3-points-docs/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.ff584096.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpoints=self.webpackChunkpoints||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/web3-points-docs/blog","blogTitle":"Blog"}')}}]);
